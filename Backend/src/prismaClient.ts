5384edb2a6ead7f2e2ec099d69669b7f24a2e0c9
Tojonirina Andrian <andrianjafiniaina21@gmail.com>
2026-01-29 14:24:10 +0300

    adding new package adapter PrismaPg then initializing it in prisma client to adapt the new version of prisma 7. Adding the adapter as an argument for the new PrismaClient

--- Backend/src/prismaClient.ts
+++ Backend/src/prismaClient.ts
@@ -1,2 +1,8 @@
+import { PrismaClient } from "./generated/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-export const prismaClient = new PrismaClient();
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL
+});
+
+export const prismaClient = new PrismaClient({ adapter });
