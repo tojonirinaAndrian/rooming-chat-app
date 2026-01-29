97af2dcad19146fc7c9d4013a89727a842df0fa7
Tojonirina Andrian <andrianjafiniaina21@gmail.com>
2026-01-29 13:34:11 +0300

    adding ws endpoint, adding prisma client, modified prisma shcmea by adding created_by,

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,13 +3,31 @@ import dotenv from "dotenv";
 import { fire } from "hono/service-worker";
 import {cors} from "hono/cors";
 import { serveStatic } from "hono/bun";
+import { upgradeWebSocket } from 'hono/bun';
+import { prismaClient } from './prismaClient';
 
 const app = new Hono();
 const FRONT_URL: string = String(process.env.FRONT_URL);
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
-})
+});
+
+// websocket
+app.get("/ws", upgradeWebSocket((c) => {
+  return {
+    onOpen(ws) {
+      console.log("WS Connected");
+    }, 
+    onMessage(event, ws) {
+      console.log("Received : ", event.data);
+      ws.send(String(event.data));
+    },
+    onClose() {
+      console.log("WS disconnected");
+    }
+  }
+}));
 
 // CORS
 app.use('*', cors({
@@ -17,11 +35,24 @@ app.use('*', cors({
   allowMethods:['GET', 'POST', 'DELETE', 'PUT'],
   allowHeaders:['Content-Type', 'Authorization'],
   credentials: true
-}))
+}));
 
-app.get("/api/createRoom", (c) => {
+// REST API
+// create_room
+app.post("/api/createRoom", async (c) => {
+  const body: {
+    room_name: string,
+    user_id: string,
+    ws: WebSocket
+  } = await c.req.json();
+  const room_name: string = body.room_name;
+  const created_by: string = body.user_id;
+  const user_ws: WebSocket = body.ws;
+
   return c.text("Creating room");
-})
+});
+
 
 fire(app);
+
 export default app;
