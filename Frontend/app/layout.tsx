70b9ca7d2c8b6bb1e9b06a8b8b937f960aa35684
Tojonirina Andrian <andrianjafiniaina21@gmail.com>
2026-02-06 17:18:15 +0300

    changed title to real TIME and not real TYPE in layout.tsx

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Real type chat app",
+  title: "Real time chat app",
   description: "Creating my first app that uses websocket",
 };
 
